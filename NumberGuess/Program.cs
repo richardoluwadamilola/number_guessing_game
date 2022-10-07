e3d726b7649db855774200b2dae51939fbabf6c7
richardoluwadamilola <oluwatobyrichard@gmail.com>
2022-10-07 12:17:25 +0100

    building my C# skills by building a number guessing game

---
+++ NumberGuess/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberGuess
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("what is your name?");
+
+            // get user input
+            string inputName = Console.ReadLine();
+
+            Console.WriteLine("Hello {0}, let's play a number guessing game", inputName);
+
+        }
+    }
+}
